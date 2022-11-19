887d4f3ce15b2a7a07b0c337023326b460ed2784
Prabudh Kumar <prabudh2002@gmail.com>
2022-11-20 01:37:14 +0530

    make word cloud for gsdmm output

--- try_gsdmm.py
+++ try_gsdmm.py
@@ -25,9 +25,11 @@ bow_corpus = [dictionary.doc2bow(doc) for doc in docs]
 
 # initialize GSDMM
 gsdmm = MovieGroupProcess(K=10, alpha=0.1, beta=0.3, n_iters=10)
+# print(gsdmm)
 
 # fit GSDMM model
 y = gsdmm.fit(docs, vocab_length)
+# print(y)
 
 # print number of documents per topic
 doc_count = np.array(gsdmm.cluster_doc_count)
@@ -38,11 +40,22 @@ top_index = doc_count.argsort()[-15:][::-1]
 print('Most important clusters (by number of docs inside):', top_index)
 
 # define function to get top words per topic
+for_wcloud = []
 def top_words(cluster_word_distribution, top_cluster, values):
     for cluster in top_cluster:
         sort_dicts = sorted(cluster_word_distribution[cluster].items(), key=lambda k: k[1], reverse=True)[:values]
+        sort_dicts = dict(sort_dicts)
+        sum = 0
+        for ky in sort_dicts:
+            sum += sort_dicts[ky]
+        for ky in sort_dicts:
+            sort_dicts[ky] /= sum
+            sort_dicts[ky] = round(sort_dicts[ky], 2)
+
+        if len(sort_dicts) == 0:
+            continue
+        for_wcloud.append(sort_dicts)
         print("\nCluster %s : %s"%(cluster, sort_dicts))
 
 # get top words in topics
 top_words(gsdmm.cluster_word_distribution, top_index, 8)
-
